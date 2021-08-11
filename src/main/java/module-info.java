b139441eb74e9e94d61359751c8fe51707d126df
kantapat <kantapat.r@ku.th>
2021-08-11 11:33:58 +0700

    new commit

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ku.cs {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens ku.cs to javafx.fxml;
-    exports ku.cs;
-    exports ku.cs.controllers;
-    opens ku.cs.controllers to javafx.fxml;
-}
